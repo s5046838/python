f27cfd32f3068d1b342da33ba9481caa758d3580
Kayn <kayn.critchley@griffith.edu.au>
2017-08-17 20:26:51 +1000

    I've added some tests to the test_ladder.py file, testing some functions in word_ladder. They need work still.

--- test_ladder.py
+++ test_ladder.py
@@ -1,13 +1,26 @@
-from word_ladder import same, build, find # Imports the functions from the word_ladder python file
-import unittest
-import re
+import unittest # module for testing purposes
+import word_ladder as wordladder # imports word_ladder.py files as wordladder
+
+
 
 
 
 class testWord(unittest.TestCase):  #creates a unittest class to initiate unit testing of functions
 
-    # tests if the self function zips the two tuples together
+    # tests if the same function zips the two tuples together
     def test_same(self, x, y):
-      self.assertIn(same(x, y))
+        self.assertIn(wordladder.same(x, y))
+    # tests if the file is empty
+    def test_file(self, file):
+        result = wordladder.file('dictionary.txt').read()
+        self.assertTrue(len(result) != 0)
+    # test if there is no file
+    def test_no_file(self):
+        result = wordladder.file('empty.txt').read()
+        self.assertTrue(len(result) == 0)
+    # def test_build(self):
+    #     result = wordladder.build('empty.txt').read()
+    # def test_find(self):
+    #     result = wordladder.find('empty.txt').read()
 
-unittest.main()
+unittest.main() # runs the tests
