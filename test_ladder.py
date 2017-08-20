ea30e304b99f1076fa8f3b494dabc0b01040bf00
s5002464 <blake.godfrey@griffithuni.edu.au>
2017-08-20 21:08:13 +1000

    Testing done with many errors Needs Fixing

--- test_ladder.py
+++ test_ladder.py
@@ -3,23 +3,25 @@ import word_ladder as wordladder # imports word_ladder.py files as wordladder
 
 class testWord(unittest.TestCase):  #creates a unittest class to initiate unit testing of functions
 
-    # tests if the same function zips the two tuples together
-    def test_same(self, x, y):
-        self.assertIn(wordladder.same(x, y))
 
-    # tests if the file is empty
-    def test_file(self, file):
-        result = wordladder.file('dictionary.txt').read()
+    def test_file(self,file):
+        result = wordladder.file("dictionary.txt").read()
         self.assertTrue(len(result) != 0)
 
-    # test if there is no file
     def test_no_file(self):
-        result = wordladder.file('empty.txt').read()
-        self.assertTrue(len(result) == 0)
+        result = wordladder.file('dictionary.txt').read()
+        self.assertFalse(len(result) == 0)
 
-    # def test_build(self):
-    #     result = wordladder.build('empty.txt').read()
-    # def test_find(self):
-    #     result = wordladder.find('empty.txt').read()
+     def test_build(self):
+         result = wordladder.build('empty.txt').read()
+     def test_find(self):
+         result = wordladder.find('empty.txt').read()
 
-unittest.main() # runs the tests
+
+        def test_same(self, x, y):
+            self.assertIn(wordladder.same(x, y))
+
+    def test_wordRemove(self,path,word):
+        self.assertIn(wordladder.wordRemove(path,word))
+
+    unittest.main(exit=False) # runs the tests
