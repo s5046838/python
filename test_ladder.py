ab506fe86256a1f3bc4bea2450408eeafb8794a3
s5002464 <blake.godfrey@griffithuni.edu.au>
2017-08-20 22:30:48 +1000

    Fixed up my tests and added new ones.

--- test_ladder.py
+++ test_ladder.py
@@ -4,24 +4,41 @@ import word_ladder as wordladder # imports word_ladder.py files as wordladder
 class testWord(unittest.TestCase):  #creates a unittest class to initiate unit testing of functions
 
 
-    def test_file(self,file):
+    def test_file(self,file): # Tests whether the dictionary.txt file works
         result = wordladder.file("dictionary.txt").read()
         self.assertTrue(len(result) != 0)
 
-    def test_no_file(self):
+    def test_no_file(self): # Tests whether the dictionary.txt file does not work
         result = wordladder.file('dictionary.txt').read()
         self.assertFalse(len(result) == 0)
 
-     def test_build(self):
+    def test_build(self): # Tests whether the wrong file is being read
          result = wordladder.build('empty.txt').read()
-     def test_find(self):
-         result = wordladder.find('empty.txt').read()
+         self.assertFalse(len(result) != 0)
+
+    def test_same(self): # Tests the inputs for the word ladder if they work
+        result = zip(1, 'one')
+        self.assertTrue(result != 0)
+
+    def test_same(self): # Tests the inputs for the word ladder if they don't
+        result = zip(1, 'one')
+        self.assertTrue(result == 0)
 
 
-        def test_same(self, x, y):
-            self.assertIn(wordladder.same(x, y))
+    def test_wordRemove(self, path, word): # Tests if the removed word is in the word path
+        result = (path == word)
+        self.assertTrue(result)
 
-    def test_wordRemove(self,path,word):
-        self.assertIn(wordladder.wordRemove(path,word))
+    def test_wordRemove(self, path, word):  # Tests if the removed word isn't in the word path
+        result = (path != word)
+        self.assertFalse(result)
+
+    def test_wordRemove(self, path, word):  # Tests if the user starting word is equal to the word user wants removed
+        result = (path != word[0])
+        self.assertTrue(result)
+
+    def test_userinput(self): #Checks if the user's input is not none
+        result = input("")
+        self.assertIsNotNone(result, '')
 
     unittest.main(exit=False) # runs the tests
